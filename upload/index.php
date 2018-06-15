699994f071df3d723b00c8226f7f70e693dc9002
Marco Di Francesco <marcodifran@gmail.com>
2018-06-15 22:08:48 +0200

    Upload folder, sort by date

--- upload/index.php
+++ upload/index.php
@@ -1,18 +1,31 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<style>
+		td {
+			padding: 10px;
+		}
+
+		tr {
+			border-bottom: 1px solid lightgray;
+		}
+
+		table {
+			border-collapse: collapse;
+		}
+		</style>
 		<title>Upload files</title>
 		<meta charset="utf-8">
 		</head>
 	<body>
 		<div style="float: left; margin-left: 20px; margin-right: 20px;">
 			<h1 style="-webkit-margin-before:0px;">Upload your files</h1>
-			<form method="post" action="upload.php" enctype="multipart/form-data">
+			<form method="post" action="postUpload.php" enctype="multipart/form-data">
 				<p><input type="file" name="fileToUpload" id="fileToUpload"></p>
 				<h3>Max size: 100MB</h3>
 				<p><input type="submit" name="submit" value="Upload file" style="width: 200px; height: 50px;"></p>
 			</form>
-			<form method="post" action="uploadLink.php" enctype="multipart/form-data">
+			<form method="post" action="linkUpload.php" enctype="multipart/form-data">
 				<br>
 				<h1>Upload Google Drive files</h1>
 				<h3>Still not working</h3>
@@ -24,20 +37,19 @@
 			<h1>Uploaded files</h1>
 			<table>
 				<?php
-				foreach ((glob('uploads/*')) as $file) {
-					echo "<tr>";
-					echo "<td style='padding-right:10px;'>";
-					echo $file;
-					echo "</td>";
-					echo "<td>";
-					echo date ("d F Y H:i:s", filemtime($file));
-					echo "</td>";
-					echo "<td>";
-					echo "<a href='".$file."' download='".$file."'>Download</a>";
-					echo "</td>";
-					echo "</tr>";
+				$files = glob('../file/*');
+
+				usort($files, function($a, $b) {
+				    return filemtime($a) < filemtime($b);
+				});
+
+				foreach($files as $file){
+			    echo "<td><a href='{$file}'>".basename($file)."</a></td>
+		      <td>".date('d/m/Y - H:i', filemtime($file))."</td>
+					</tr>";
 				}
 				?>
+
 			</table>
 		</div>
 	</body>
---
+++ upload/postUpload.php
@@ -0,0 +1,23 @@
+<?php
+$target_file = "uploads/" . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 1000000000000) {
+    echo "Il fle è troppo grande";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 1) {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "Il file <b>". basename( $_FILES["fileToUpload"]["name"]). "</b> è stato uppato.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+?>
