939820bb5d84dc117745028f4e7c45a98e3877c1
Marco <marcodifran@gmail.com>
2018-11-19 17:53:26 +0100

    Removed sorting code

--- upload/index.php
+++ upload/index.php
@@ -31,42 +31,19 @@
 				});
 			});
 			
-			/*
-			// version of sorting in a file, not working 
-
 			$("#searchFiles").keyup(function(){ // when there is an input update table
 				value = $(this).val();
-				// check which radio box is checked
-				if(document.getElementById("name").checked == true){
-					var sort = "name";
-				} else if(document.getElementById("date").checked == true){
-					var sort = "date";
-				} else if(document.getElementById("size").checked == true){
-					var sort = "size";
-				} else {
-					var sort = "error";
-				}
 		    $.ajax({
 					type:"POST",
 					url: "search.php",
 					data:{
 						'searchInput':value,
-						'sort':sort
 					},
 					success: function(result){
-						var table = document.getElementById('fileTable');
-							row = table.insertRow(1);
-							cell1 = row.insertCell(0);
-							cell2 = row.insertCell(1);
-
-							cell1.innerHTML = value;
-							cell2.innerHTML = sort;
-
-		        // $("#searchOutput").html(result);
+						$("#searchOutput").html(result);
 		    	}
 				});
 			});
-			*/
 		});
 		</script>
 
@@ -93,24 +70,6 @@
 				<h1>Uploaded files</h1>
 				<input type="text" width="100px" placeholder="Search" id="searchFiles">
 			</div>
-			<table id="fileTable">
-				<form id='sort'>
-					<tr>
-						<th>
-							Name
-							<input type='radio' name='sort' value='name' id="name">
-						</th>
-						<th>
-							Date
-							<input type='radio' name='sort' value='date' id="date" checked>
-						</th>
-						<th>
-							Size
-							<input type='radio' name='sort' value='size' id="size">
-						</th>
-					</tr>
-				</form>
-			</table>
 			<table>
 				<div id="searchOutput"></div>
 			</table>
