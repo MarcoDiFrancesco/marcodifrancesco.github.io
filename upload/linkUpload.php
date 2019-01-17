38307d74f74a22ae918d4a695890f81402f8ff18
Marco <marcodifran@gmail.com>
2019-01-17 15:10:56 +0100

    Fix names of files

--- upload/linkUpload.php
+++ upload/linkUpload.php
@@ -1,7 +1,7 @@
 <?php
 $link = $_POST["linkToUpload"];
 $filename = basename($link);
-
+$filename = str_replace('%20', ' ', $filename);
 $upload = file_put_contents("../file/".$filename, fopen($link, 'r'));
 
 // https://drive.google.com/uc?authuser=0&id=1Cphvdy85UnC4ne2UtioRJ1YNkxc4RQpl&export=download
