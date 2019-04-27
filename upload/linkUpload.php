59a44056ffdd87858b48ea7116601cdff609bddc
Marco <marcodifran@gmail.com>
2019-04-27 12:20:00 +0200

    Changed link from www to non-www

--- upload/linkUpload.php
+++ upload/linkUpload.php
@@ -5,6 +5,6 @@ $filename = str_replace('%20', ' ', $filename);
 $upload = file_put_contents("../file/".$filename, fopen($link, 'r'));
 
 // https://drive.google.com/uc?authuser=0&id=1Cphvdy85UnC4ne2UtioRJ1YNkxc4RQpl&export=download
-// https://www.marcodifrancesco.com/upload/uploads/100%20mega.txt
+// https://marcodifrancesco.com/upload/uploads/100%20mega.txt
 header("Location: index.php");
 ?>
